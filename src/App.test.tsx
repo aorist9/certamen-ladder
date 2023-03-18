47b923127f387813719962d8301207e319a6279a
Andrew Rist <aorist9@gmail.com>
2023-03-17 20:24:27 -0500

    test: fixes broken App test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-describe('App', () => {
-	test('renders header', () => {
+describe("App", () => {
+	test("renders header", () => {
 		render(<App />);
-		const linkElement = screen.getByText(/Certamen Ladder/i);
-		expect(linkElement).toBeInTheDocument();
+		const linkElement = screen.getAllByText(/Certamen Ladder/i);
+		expect(linkElement.length).toBeGreaterThan(0);
 	});
 
-	test('includes sidebar', () => {
+	test("includes sidebar", () => {
 		render(<App />);
 		expect(screen.getByText(/create a new ladder/i)).toBeInTheDocument();
 	});
