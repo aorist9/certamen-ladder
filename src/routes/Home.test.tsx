26d66acc306b5a81a6e18f1c231a9b3801877222
Andrew Rist <aorist9@gmail.com>
2023-03-17 20:51:55 -0500

    test: fixed another test

--- src/routes/Home.test.tsx
+++ src/routes/Home.test.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Home from './Home';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Home from "./Home";
 
-describe('Home', () => {
-	test('should show static text', () => {
+describe("Home", () => {
+	test("should show static text", () => {
 		render(<Home />);
-		expect(screen.getByText(/Home/i)).toBeInTheDocument();
+		expect(screen.getByText(/About This Tool/i)).toBeInTheDocument();
 	});
 });
