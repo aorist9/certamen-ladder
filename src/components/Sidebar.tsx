c65b0e05e174c8c6b824c2c7a1564ce902961db2
Andrew Rist <aorist9@gmail.com>
2023-07-21 17:54:15 -0500

    fix: hides sidebar in mobile view again

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -13,22 +13,25 @@ const Sidebar = ({ visible }: SidebarProps) => (
 		className={`Sidebar ${
 			visible ? "sidebar-mobile-visible" : "sidebar-mobile-hidden"
 		}`}
-		style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
 	>
-		<nav>
-			<ul>
-				<li>
-					<Link to="/">Home</Link>
-				</li>
-				<li>
-					<Link to="/create">Create a New Ladder</Link>
-				</li>
-				<li>
-					<Link to="/ladders">View Existing Ladders</Link>
-				</li>
-			</ul>
-		</nav>
-		{features.publishLadder ? <PublishLadder /> : ""}
+		<section
+			style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+		>
+			<nav>
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/create">Create a New Ladder</Link>
+					</li>
+					<li>
+						<Link to="/ladders">View Existing Ladders</Link>
+					</li>
+				</ul>
+			</nav>
+			{features.publishLadder ? <PublishLadder /> : ""}
+		</section>
 	</section>
 );
 
