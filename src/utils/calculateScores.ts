5df374022d5f323bb17316fc99914e82bda85e2f
Andrew Rist <aorist9@gmail.com>
2025-05-10 15:20:34 -0500

    refactor: extracts functions from calculateScores

--- src/utils/calculateScores.ts
+++ src/utils/calculateScores.ts
@@ -1,4 +1,5 @@
-import { Ladder } from "../types/LadderType";
+import { Division, Ladder } from "../types/LadderType";
+import { RoomV2 } from "../types/Matches";
 
 export type ScoreRow = {
 	team: string;
@@ -25,16 +26,11 @@ export const sortScores = (score1: ScoreRow, score2: ScoreRow) => {
 	return score2.total - score1.total;
 };
 
-const calculateScores = (
+const initializeScoreRows = (
 	ladder: Ladder,
-	divisionNumber: number
+	division: Division
 ): ScoreRow[] => {
-	const division = ladder.divisions?.[divisionNumber];
-	if (!division) {
-		return [];
-	}
-
-	let result: ScoreRow[] = Object.values(division.teams).map(team =>
+	return Object.values(division.teams).map(team =>
 		ladder.isSwiss()
 			? {
 					team,
@@ -49,50 +45,67 @@ const calculateScores = (
 					total: 0
 			  }
 	);
+};
+
+const updateScoresForMatch = (result: ScoreRow[], match: RoomV2) => {
+	match.teams.forEach(t => {
+		const boardTeam = result.find(team => t.team === team.team);
+		if (boardTeam) {
+			boardTeam.roundScores.push(t.score);
+			if (t.swissPoints) {
+				boardTeam?.roundSwiss?.push(t.swissPoints);
+			}
+			if (t.score) {
+				boardTeam.total += t.score;
+			}
+			if (boardTeam.swissTotal !== undefined && t.swissPoints) {
+				boardTeam.swissTotal += t.swissPoints;
+			}
+		}
+	});
+};
+
+const calculateSOS = (result: ScoreRow[], division: Division): ScoreRow[] => {
+	return result.map(row => ({
+		...row,
+		sos: division.matches?.reduce((acc, round) => {
+			const match = round.find(room =>
+				room.teams.some(t => t.team === row.team)
+			);
+			if (match) {
+				return match.teams
+					.filter(team => team.team !== row.team)
+					.reduce((sos, team) => {
+						const foundTeam = result.find(r => r.team === team.team);
+						return sos + (foundTeam?.swissTotal || 0);
+					}, acc);
+			}
+			return acc;
+		}, 0)
+	}));
+};
+
+const calculateScores = (
+	ladder: Ladder,
+	divisionNumber: number
+): ScoreRow[] => {
+	const division = ladder.divisions?.[divisionNumber];
+	if (!division) {
+		return [];
+	}
+
+	let result: ScoreRow[] = initializeScoreRows(ladder, division);
 
 	if (division.matches) {
 		division.matches.forEach(round => {
 			round.forEach(match => {
-				match.teams.forEach(t => {
-					const boardTeam = result.find(team => t.team === team.team);
-					if (boardTeam) {
-						boardTeam.roundScores.push(t.score);
-						if (t.swissPoints) {
-							boardTeam?.roundSwiss?.push(t.swissPoints);
-						}
-
-						if (t.score) {
-							boardTeam.total += t.score;
-						}
-						if (boardTeam.swissTotal !== undefined && t.swissPoints) {
-							boardTeam.swissTotal += t.swissPoints;
-						}
-					}
-				});
+				updateScoresForMatch(result, match);
 			});
 		});
 	}
 
 	if (ladder.isSwiss() && division.matches) {
-		result = result.map(row => ({
-			...row,
-			// @ts-ignore
-			sos: division.matches.reduce((acc, round) => {
-				const match = round.find(
-					room => !!room.teams.find(t => t.team === row.team)
-				);
-				if (match && match.teams[0].score !== undefined) {
-					return match.teams
-						.filter(team => team.team !== row.team)
-						.reduce((sos, team) => {
-							const foundTeam = result.find(r => r.team === team.team);
-							return sos + (foundTeam?.swissTotal || 0);
-						}, acc);
-				} else {
-					return acc;
-				}
-			}, 0)
-		}));
+		result = calculateSOS(result, division);
 	}
 
 	return result;
